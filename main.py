dff4f49b690955599df22a5df1e715249c25ac07
Nilesh Vaidya <nileshavaidya96@gmail.com>
2025-01-31 15:03:25 +0530

    average fun added

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import os
 import logging
 
 
-<<<<<<< HEAD
 def addtion(a,b,c,e):
     add=a + b + c +e
-=======
-def addtion(a,b,c, d):
-    add=a + b + c + d
->>>>>>> 3a56c44ec0021ed6567f13d7b236898dfd1b8c61
     return add
     
 def summation(x,y):
     summ=x+y
     return summ
+
+def average(x,y):
+    avgg=(x+y)/2
+    return avgg
